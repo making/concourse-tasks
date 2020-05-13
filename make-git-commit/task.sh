34aed501578a1527a9e969caea8b6606cd6df41a
Toshiaki Maki <tmaki@pivotal.io>
2020-05-13 23:47:42 +0900

    Update task.sh

--- make-git-commit/task.sh
+++ make-git-commit/task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -eux
 
 # https://docs.pivotal.io/platform-automation/v4.3/tasks.html#make-git-commit
 
@@ -7,15 +7,9 @@ git clone repository repository-commit
 
 
 FILE_DESTINATION_PATH="repository-commit/$FILE_DESTINATION_PATH"
-destination_directory=$(dirname "$FILE_DESTINATION_PATH")
+mkdir -p $FILE_DESTINATION_PATH
 
-if [ ! -d "$destination_directory" ]; then
-  echo "Directory $destination_directory does not exist in repository, creating it..."
-  mkdir -p "$destination_directory";
-fi;
-
-cp -R file-source/$FILE_SOURCE_PATH \
-   "$FILE_DESTINATION_PATH"
+cp -R file-source/$FILE_SOURCE_PATH $FILE_DESTINATION_PATH
 cd repository-commit
 git config user.name "$GIT_AUTHOR_NAME"
 git config user.email "$GIT_AUTHOR_EMAIL"
